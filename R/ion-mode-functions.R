7a3d5038b3c7879faced9ecc4047cdbd6f54eaef
Mosley <Mosley.Jonathan@epa.gov>
2018-10-02 18:21:24 -0400

    Addressed CRAN notes

--- R/ion-mode-functions.R
+++ R/ion-mode-functions.R
@@ -122,8 +122,9 @@ remove_ion_dup = function(Peak.list, Key.list, tbl.id, ...) {
 #' @param Peak.list a named list of data frames \(one per ionization mode\) containing intensity matrices across all study samples and Pooled QCs.
 #' Names must be c('Positive','Negative').  Alternatively may use existing database connections by setting to NULL and specifying database parameters with ...
 #' @param tbl.id character vector of table names to draw from database.  First value should be table name for positive mode, second should be table name for negative mode. Default is NULL
+#' ... arguments to pass to database functions
 #' @return NULL testing
-pre_combine_ion_modes = function(Peak.list = NULL, tbl.id) {
+pre_combine_ion_modes = function(Peak.list = NULL, tbl.id, ...) {
   if (missing(tbl.id))
     tbl.id = NULL
   if (is.null(tbl.id) && is.null(Peak.list)) {
