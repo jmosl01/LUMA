182c337b88b3ccb0bff5583e8c0fdae73fbf195d
Mosley <Mosley.Jonathan@epa.gov>
2019-11-06 17:24:41 -0500

    Added package tests for utility functions

---
+++ tests/testthat/test-utilities.R
@@ -0,0 +1,23 @@
+context("test-utilities")
+
+test_that("Preprocess files names are correct", {
+  expect_match(.set_PreProcessFileNames("Positive",TRUE)[[1]],"Blanks_Pos")
+  expect_match(.set_PreProcessFileNames("Positive",TRUE)[[2]],"Blanks_Pos")
+  expect_match(.set_PreProcessFileNames("Negative",TRUE)[[1]],"Blanks_Neg")
+  expect_match(.set_PreProcessFileNames("Negative",TRUE)[[2]],"Blanks_Neg")
+  expect_match(.set_PreProcessFileNames("Positive",FALSE)[[1]],"objects_Pos")
+  expect_match(.set_PreProcessFileNames("Positive",FALSE)[[2]],"objects_Pos")
+  expect_match(.set_PreProcessFileNames("Negative",FALSE)[[1]],"objects_Neg")
+  expect_match(.set_PreProcessFileNames("Negative",FALSE)[[2]],"objects_Neg")
+})
+
+test_that("Two elements are the same", {
+  expect_true(.SameElements(c(1, 2, 3), c(1, 3, 2)))
+  expect_false(.SameElements(c(1, 2, 3), c(1, 1, 3, 2)))
+})
+
+test_that("EICs are converted to numbers", {
+  expect_equal(.convert_EIC("1"),1)
+  expect_null(.convert_EIC(NULL))
+
+})
