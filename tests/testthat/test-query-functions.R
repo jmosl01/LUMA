c416602e25b381c1c19c83b23708856552f040f8
Mosley <Mosley.Jonathan@epa.gov>
2020-05-26 21:57:56 -0400

    match_Annotation now requires Ion.Mode column

--- tests/testthat/test-query-functions.R
+++ tests/testthat/test-query-functions.R
@@ -2,24 +2,27 @@ context("test-query-functions")
 
 test_that("mz features are annotated correctly", {
 
-  if(require(lcmsfishdata, quietly = TRUE)) {
+  if(require(RSQLite, quietly = TRUE)) {
     Peak.list <- Peaklist_Pos$From_CAMERA
-    file <- system.file('extdata/primary_adducts_pos.csv', package = "lcmsfishdata")
+    file <- system.file('extdata/primary_adducts_pos.csv', package = "LUMA")
     rules <- read.table(file, header = TRUE, sep = ",")
-    file2 <- system.file('extdata/Search_Parameters.txt', package = "lcmsfishdata")
+    file2 <- system.file('extdata/Search_Parameters.txt', package = "LUMA")
     search.par <- read.table(file2, header = TRUE, sep = "\t")
-    file3 <- system.file('extdata/Annotated_library.csv', package = "lcmsfishdata")
+    file3 <- system.file('extdata/Annotated_library.csv', package = "LUMA")
     temp.library <- read.table(file3, header = TRUE, sep = ",")
-    Library.phenodata = temp.library[,-which(colnames(temp.library) %in% c("Name","Formula","Molecular.Weight","RT..Min."))]
-    Annotated.library = cbind.data.frame(Name = temp.library[["Name"]],
+    Library.phenodata <- temp.library[,-which(colnames(temp.library) %in% c("Name","Formula","Molecular.Weight","RT..Min.","Ion.Mode"))]
+    Annotated.library <- cbind.data.frame(Name = temp.library[["Name"]],
                                          Formula = temp.library[["Formula"]],
                                          Molecular.Weight = temp.library[["Molecular.Weight"]],
                                          RT..Min. = temp.library[["RT..Min."]],
-                                         Library.phenodata)
+                                         Ion.Mode = temp.library[["Ion.Mode"]])
 
     lib_db <- connect_libdb(lib.db = "Annotated_library", mem = TRUE)
-    test <- match_Annotation(Peak.list = Peak.list, Annotated.library = Annotated.library, rules = rules, search.par = search.par, IonMode = "Positive", lib_db = lib_db)
+    test <- match_Annotation(Peak.list = Peak.list, Annotated.library = Annotated.library,
+                             Library.phenodata = Library.phenodata, rules = rules,
+                             search.par = search.par, IonMode = "Positive", lib_db = lib_db)
     expect_equal(grep("GUANOSINE",test$Name),19)
     expect_equal(grep("INOSINE",test$Name),c(15,21,22))
+    dbDisconnect(lib_db)
   }
 })
